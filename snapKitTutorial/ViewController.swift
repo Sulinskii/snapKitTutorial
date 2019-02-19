e28f92ffd09d2210b3309d52515abf906aef5cad
Artur Sulinski <artur.sulinski@brightinventions.pl>
2019-02-19 17:02:04 +0100

    Initial Commit

---
+++ snapKitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapKitTutorial
+//
+//  Created by Artur Sulinski on 19/02/2019.
+//  Copyright © 2019 Artur Sulinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ snapKitTutorialTests/snapKitTutorialTests.swift
@@ -0,0 +1,34 @@
+//
+//  snapKitTutorialTests.swift
+//  snapKitTutorialTests
+//
+//  Created by Artur Sulinski on 19/02/2019.
+//  Copyright © 2019 Artur Sulinski. All rights reserved.
+//
+
+import XCTest
+@testable import snapKitTutorial
+
+class snapKitTutorialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
